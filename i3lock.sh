6c953e8b9b6e7ed5dd09372834347904f9f6648a
dfuse06 <dfuse06@gmail.com>
2016-12-24 23:47:06 -0600

    i3locker "mode" to handle system shutdown, reboot, etc

--- i3lock.sh
+++ i3lock.sh
@@ -1,8 +1,45 @@
 #!/bin/sh
 
-# Take a screenshot
-scrot ~/Pictures/screen_locked.png
+lock() {
+    i3lock
+}
 
-convert ~/Pictures/screen_locked.png -blur 0x5 ~/Pictures/screen_locked.png
-i3lock -i ~/Pictures/screen_locked.png
-#rm ~/Pictures/screen_locked.png
+case "$1" in
+    lock)
+         # Take a screenshot
+        scrot ~/Pictures/screen_locked.png
+        convert ~/Pictures/screen_locked.png -blur 0x5 ~/Pictures/screen_locked.png
+        i3lock -i ~/Pictures/screen_locked.png 
+        rm ~/Pictures/screen_locked.png
+        ;;
+    logout)
+        i3-msg exit
+        ;;
+    suspend)
+        # Take a screenshot
+        scrot ~/Pictures/screen_locked.png
+        convert ~/Pictures/screen_locked.png -blur 0x5 ~/Pictures/screen_locked.png
+        i3lock -i ~/Pictures/screen_locked.png 
+        rm ~/Pictures/screen_locked.png
+        systemctl suspend
+        ;;
+    hibernate)
+        # Take a screenshot
+        scrot ~/Pictures/screen_locked.png
+        convert ~/Pictures/screen_locked.png -blur 0x5 ~/Pictures/screen_locked.png
+        i3lock -i ~/Pictures/screen_locked.png 
+        rm ~/Pictures/screen_locked.png
+        systemctl hibernate
+        ;;
+    reboot)
+        systemctl reboot
+        ;;
+    shutdown)
+        systemctl poweroff
+        ;;
+    *)
+        echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
+        exit 2
+esac
+
+exit 0
